35b7c51ec00e9a4f85eff2d54874f492a85cc97f
Levi <96695555+Levi0100@users.noreply.github.com>
2022-09-19 10:07:05 -0300

    Revert "Attachment Object"

--- index.js
+++ index.js
@@ -1,6 +1,5 @@
 module.exports = {
     Embed: require('./lib/structures/Embed.js'),
     Button: require('./lib/structures/Button.js'),
-    SelectMenu: require('./lib/structures/SelectMenu.js'),
-    Attachment: require('./lib/structures/Attachment.js')
+    SelectMenu: require('./lib/structures/SelectMenu.js')
 }
